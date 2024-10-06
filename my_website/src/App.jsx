182fb4a6186563f49e18d43b678fd2e43334cd31
jaylyn36 <jaylyng02@icloud.com>
2024-10-06 19:25:05 -0400

    Minor changes

--- my_website/src/App.jsx
+++ my_website/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="bg-black h-screen flex items-center justify-center">
       <Start />   {/* implement route handling to redirect to the next page after action in "Start.jsx" */}
+      
     </div>
   );
 }
--- my_website/src/pages/Start.jsx
+++ my_website/src/pages/Start.jsx
@@ -1,7 +1,7 @@
 const Start = () => {
     return ( 
-        <div className="text-white text-3xl" onClick={""}>      {/* implement function to handle clicking on the first page */}
-            Click anywhere to get started
+        <div className="text-white text-5xl" onClick={""}>      {/* implement function to handle clicking on the first page to route the user to the next page */}
+            Click anywhere to get started                       {/* animate the text somehow? flashing bar at the end of the line like a terminal screen?  */}
         </div>
      );
 }
