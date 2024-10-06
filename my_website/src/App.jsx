256d9f4279b1e1d4d058cb4d57739fe673ffde64
jaylyn36 <jaylyng02@icloud.com>
2024-10-06 19:11:00 -0400

    First commit

---
+++ my_website/src/App.jsx
@@ -0,0 +1,11 @@
+import Start from "./pages/Start";
+
+function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <Start />
+    </div>
+  );
+}
+
+export default App;
---
+++ my_website/src/pages/Start.jsx
@@ -0,0 +1,9 @@
+const Start = () => {
+    return ( 
+        <div className="text-white text-3xl">
+            Click anywhere to get started
+        </div>
+     );
+}
+ 
+export default Start;
