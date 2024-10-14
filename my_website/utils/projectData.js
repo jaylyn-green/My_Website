446b78f18078babdb5b9b930ecdb5daff0f9a0de
jaylyn36 <jaylyng02@icloud.com>
2024-10-14 14:06:54 -0400

    Project description changes for 'FitnessTracker' in 'projectData.js'

--- my_website/utils/projectData.js
+++ my_website/utils/projectData.js
@@ -3,7 +3,7 @@ import { HTML, CSS, REACT, NODEJS, DB } from "./icons";
 
 export const projectData = {
     FitnessTracker: {
-        description: "A web application that helps users track their fitness activities and goals. This application allows the user to input their run times, distance, and weight training exercises and is able to set goals based on the data the provide. This application uses Html, CSS, JavaScript, React, Node.js and MongoDb.",
+        description: "A web application that helps users track their fitness activities and goals. This application allows the user to input their run times, distance, and weight training exercises and is able to set goals based on the data the provide. This project is not completed just yet but it will be soon. This application uses Html, CSS, JavaScript, React, Node.js and MongoDb.",
         images: [], // put images later
         icons: [HTML, CSS, REACT, NODEJS, DB],
         link: "https://github.com/jaylyn-green/fitness_tracker_fullstack"
